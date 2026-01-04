7ff6f68270b4a31b560727990a7d0df2388864f7
PRINCE-OBOT <princessamuel45@gmail.com>
2026-01-04 15:48:24 +0100

    Add method for appending node

---
+++ webpack.common.js
@@ -0,0 +1,31 @@
+const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: { index: "./src/index.js" },
+  output: {
+    filename: "[name].js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: "./src/template.html"
+    })
+  ],
+  devServer: {
+    open: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(png|svg|jpeg|jpg|gif)$/i,
+        type: "asset/resource"
+      },
+      {
+        test: /\.(woff|woff2)$/i,
+        type: "asset/resource"
+      }
+    ]
+  }
+};
